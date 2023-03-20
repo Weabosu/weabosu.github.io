b84b0933b8ae524ebe90e7eb322009435951fe52
Weabosu <122533764+Weabosu@users.noreply.github.com>
2023-03-20 18:13:58 +0100

    Add files via upload

---
+++ docs/Lbp.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+var i, checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+function save() {
+    for (i = 0; i < checkboxes.length; i++) {
+        localStorage.setItem(checkboxes[i].value, checkboxes[i].checked); 
+    }
+}
+
+function load() {
+    for (i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = localStorage.getItem(checkboxes[i].value) === 'true' ? true:false;
+    }
+} 
